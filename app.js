b30fff1b64753f88a23a732bd17f421b8cac1baa
SergSGA13 <sergsga13@gmail.com>
2026-06-22 20:23:45 +0200

    Remove futures-strategy LiveChart mount condition

--- app.js
+++ app.js
@@ -83,7 +83,6 @@ function navigate(pageId) {
   if (pageId === 'stats-l30d') { loadPnlL30dFromSignals('pnlChartL30d', 'l30d'); renderL30dTables(); }
   if (pageId === 'stats-all')  { loadPnlAllFromSignals('pnlChartAll', 'allp'); renderAllTables(); renderMonthlyWrChart(); }
   if (pageId === 'indicators')       { window.LiveChart && LiveChart.mount('liveChartIndicators'); }
-  if (pageId === 'futures-strategy') { window.LiveChart && LiveChart.mount('liveChartStrategy'); }
 
   if (tg) tg.HapticFeedback?.impactOccurred('light');
 }
